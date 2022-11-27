f9dc0aff82649427eb8fd84a045f1f4f993a812e
beautytasara27 <beautytasara@gmail.com>
2022-11-27 21:42:59 +0200

    bug fix: dev env

--- Startup.cs
+++ Startup.cs
@@ -40,14 +40,14 @@ namespace authentication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+           // if (env.IsDevelopment())
+            //{
                 using var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
                 serviceScope.ServiceProvider.GetService<DatabaseContext>().Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "authentication v1"));
-            }
+           // }
           
             app.UseHttpsRedirection();
 
